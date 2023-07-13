925b3b9557c9eeafea88d3dcfb9e5952a65a0974
Miquel <trazomobile4@gmail.com>
2023-07-13 15:10:16 +0200

    Fixed ResponseDTO bug

--- SmartWallet.ApplicationService/Dto/Response/WalletResponseDto.cs
+++ SmartWallet.ApplicationService/Dto/Response/WalletResponseDto.cs
@@ -1,12 +1,10 @@
-﻿using SmartWallet.Infrastructure.DataModels;
-
-namespace SmartWallet.ApplicationService.Dto.Response
+﻿namespace SmartWallet.ApplicationService.Dto.Response
 {
     public class WalletResponseDto
     {
         public int Id { get; set; }
         public decimal Balance { get; set; }
         public CoinResponseDto Coin { get; set; } = new CoinResponseDto();
-        public List<BalanceHistoric> BalanceHistorics { get; set; } = new List<BalanceHistoric>();
+        public List<BalanceHistoricResponseDto> BalanceHistorics { get; set; } = new List<BalanceHistoricResponseDto>();
     }
 }
